e874ee6bcce6f4f323fb8b8489b488ee789a73d4
cancan.feng <fss_0214@163.com>
2017-11-08 18:18:36 +0800

    footer 筛选添加

--- src/app.js
+++ src/app.js
@@ -4,21 +4,28 @@ import Footer from "components/Footer";
 require("style/base.css");
 require("style/index.css");
 
-class App extends React.Component{
-    constructor(props){
+class App extends React.Component {
+    constructor(props) {
         super(props);
         this.state = {
-            todosData:[], //{id,value,hasCompleted}
-            inputVal:''
-        }
+            todosData: [], //{id,value,hasCompleted}
+            inputVal: '',
+            view: 'all'
+        };
         this.handleKeyDownPost = this.handleKeyDownPost.bind(this);
         this.inputChange = this.inputChange.bind(this);
         this.onDestroy = this.onDestroy.bind(this);
         this.onClearCompleted = this.onClearCompleted.bind(this);
         this.toggleAll = this.toggleAll.bind(this);
         this.onToggle = this.onToggle.bind(this);
+        this.changeView = this.changeView.bind(this);
+    }
+    //footer tab选中
+    changeView(view) {
+        this.setState({view})
     }
 
+    //输入内容时
     inputChange(ev) {
         this.setState({
             inputVal: ev.target.value
@@ -61,11 +68,9 @@ class App extends React.Component{
         todosData = todosData.map(elt => {
             if (elt.id === todo.id) {
                 elt.hasComplated = !elt.hasComplated;
-                console.log(elt.hasComplated)
             }
             return elt;
         });
-
         this.setState({todosData});
 
     }
@@ -76,21 +81,46 @@ class App extends React.Component{
         todosData = todosData.filter((elt) => {
             return elt.id !== todoV.id;
         });
-        this.setState({todosData: todosData});
+        this.setState({todosData});
     }
 
-    //清楚列表
+    //清除列表
     onClearCompleted() {
         let {todosData} = this.state;
         todosData = todosData.filter((elt) => {
             return !elt.hasComplated;
-        })
-        this.setState = ({todosData})
+        });
+        this.setState({todosData})
     }
-    render(){
-        let {handleKeyDownPost, onDestroy, onClearCompleted, inputChange, toggleAll,onToggle} = this;
-        let {todosData,inputVal} = this.state;
-        let items = todosData.map((elt, i) => {
+
+    render() {
+        let {handleKeyDownPost, onDestroy, onClearCompleted, inputChange, toggleAll, onToggle, changeView} = this;
+        let {todosData, inputVal, view} = this.state;
+        //初始组件空对象
+        let items = null,
+            footer = null,
+            itemsBox = null;
+        //无勾选状态的总是
+        let leftCount = todosData.length;
+        //footer下tab筛选
+        items = todosData.filter(elt=>{
+            console.log(111)
+            //如果为勾选，总数减减
+            if (elt.hasComplated == true) leftCount--;
+            switch (view){
+                //未勾选
+                case 'active':
+                    return !elt.hasComplated;
+                //已勾选
+                case 'completed':
+                    return elt.hasComplated;
+                //all
+                default:
+                    return true;
+            }
+        });
+        //数据列表
+        items = items.map((elt, i) => {
             return (
                 <Item
                     {...{
@@ -101,20 +131,38 @@ class App extends React.Component{
                     key={i}
                 />
             )
-        })
-        return (
-            <div>
-                <header className="header">
-                    <h1>todos</h1>
-                    <input type="text" className="new-todo" value={inputVal} onChange={inputChange}  onKeyDown={handleKeyDownPost}/>
-                </header>
+        });
+        //footer、数据列表显示
+        if (todosData.length) {
+            itemsBox = (
                 <section className="main">
-                    <input type="checkbox" className="toggle-all" onChange={toggleAll} />
+                    <input type="checkbox" checked={leftCount === 0} className="toggle-all" onChange={toggleAll}/>
                     <ul className="todo-list">
                         {items}
                     </ul>
                 </section>
-                <Footer/>
+            );
+            footer = (
+                <Footer
+                    {...{
+                        leftCount,
+                        showClearButton: leftCount < todosData.length,
+                        onClearCompleted,
+                        view,
+                        changeView
+                    }}
+                />
+            );
+        }
+        return (
+            <div>
+                <header className="header">
+                    <h1>todos</h1>
+                    <input type="text" className="new-todo" value={inputVal} onChange={inputChange}
+                           onKeyDown={handleKeyDownPost}/>
+                </header>
+                {itemsBox}
+                {footer}
             </div>
         )
     }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,34 +1,64 @@
-export default class Footer extends React.Component{
-    constructor(props){
+let propType = {
+    leftCount: PT.number,
+    showClearButton: PT.bool,
+    //oneOf只接受指定的值
+    view: PT.oneOf(['all', 'active', 'completed']),
+    onClearCompleted: PT.func,
+    changeView: PT.func
+};
+export default class Footer extends React.Component {
+    constructor(props) {
         super(props);
     }
-    render(){
-        return(
-            <footer className="footer">
-                <span className="todo-count"></span>
-                <strong>0</strong>
-                <span>item left</span>
-                <ul className="filters">
-                    <li>
-                        <a href="#/all"></a>
-                    </li>
-                    <li>
-                        <a href="#/active"></a>
-                    </li>
-                    <li>
-                        <a href="#/completed"></a>
-                    </li>
-                    <li>
-                        <a href="#/clear-completed"></a>
-                    </li>
-                </ul>
-                <button className="clear-completed">
+
+    render() {
+        let {
+            leftCount,
+            showClearButton,
+            view,
+            changeView,
+            onClearCompleted
+        } = this.props;
+        var clearBtn = null;
+        if (showClearButton) {
+            clearBtn = (
+                <button
+                    className="clear-completed"
+                    onClick={onClearCompleted}
+                >
                     clear add completed
                 </button>
+            )
+        }
+        return (
+            <footer className="footer">
+                <span className="todo-count">
+                    <strong>{leftCount}</strong>
+                    <span>item left</span>
+                </span>
+                <ul className="filters">
+                    <li>
+                        <a href="#/all"
+                           className={view === 'all' ? 'selected' : ''}
+                           onClick={ev => changeView('all')}
+                        >全部</a>
+                    </li>
+                    <li>
+                        <a href="#/active"
+                           className={view === 'active' ? 'selected' : ''}
+                           onClick={ev => changeView('active')}
+                        >未勾选的</a>
+                    </li>
+                    <li>
+                        <a href="#/completed"
+                           className={view === 'completed' ? 'selected' : ''}
+                           onClick={ev => changeView('completed')}
+                        >已勾选的</a>
+                    </li>
+                </ul>
+                {clearBtn}
             </footer>
-        )
+        );
     }
-
-
-
 }
+Footer.propTypes = propType;
